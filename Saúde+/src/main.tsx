5c004a40144e6e90c097de2b72afc55097081dba
Ryan Vetoriano <vetorianosilva@gmail.com>
2025-09-28 21:03:02 -0300

    Adicionando router dom no main

--- Saúde+/src/main.tsx
+++ Saúde+/src/main.tsx
@@ -2,8 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
+import Error from './routes/Error/index.tsx';
+import Home from './routes/Home/index.tsx';
+import Login from "./routes/Login/index.tsx";
+import Register from "./routes/Register/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login/> },
+      { path: "/register", element: <Register/> },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
