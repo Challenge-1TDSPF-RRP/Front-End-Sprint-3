3ef20b05062b0a479ad82d30e53debd91a60efc9
PietroDonella <pietrosalomao1@gmail.com>
2025-10-01 14:43:47 -0300

    Estilização do Header finalizada.

--- saude-mais/src/components/Header/Header.tsx
+++ saude-mais/src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import Menu from "../Menu/Menu";
 
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between h-[20vh] bg-blue-400 shadow-md">
+      {/* Título à esquerda ocupando toda a altura */}
+      <h1 className="flex items-center justify-center bg-blue-500 text-white font-bold text-4xl h-full px-10">
+        Saúde+
+      </h1>
 
-export default function Header(){
-    
-    return( 
-        <header>
-            <h1>Saúde+</h1>
-            <Menu/>
-        </header> 
-    );
+      {/* Menu à direita */}
+      <div className="flex items-center pr-10">
+        <Menu />
+      </div>
+    </header>
+  );
 }
